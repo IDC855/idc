d51326bd83f7c683c9ab9b0a4b1bd00b7a88ebbb
IDC855 <islamicdigitalcoin3@gmail.com>
2025-08-10 23:56:59 +0100

    Update firebaseConfig.js
    database change

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD9Mo5Puzqrl0NV_nLABRtvzC47K61HJOs",
-  authDomain: "idcn-7588b.firebaseapp.com",
-  projectId: "idcn-7588b",
-  storageBucket: "idcn-7588b.firebasestorage.app",
-  messagingSenderId: "367230424754",
-  appId: "1:367230424754:web:2253bdde2a6e8bb9aa5bab"
+   apiKey: "AIzaSyCd9gZhTzYBk_cVDrfDlKs8atTy1l6f71U",
+  authDomain: "newidc-95145.firebaseapp.com",
+  projectId: "newidc-95145",
+  storageBucket: "newidc-95145.firebasestorage.app",
+  messagingSenderId: "336815220136",
+  appId: "1:336815220136:web:09f10d3864cffe3d1ea99d"
   };
